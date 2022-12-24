75178c2a248457fa5dff9f592a5a223fd9e001b3
Everton Rocha <everton.srocha@gmail.com>
2022-12-24 13:24:49 +0000

    Exercicios sobre Triângulos e definições

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const prompt = require("prompt-sync")();
+const prompt = require('prompt-sync')();
 
 
-var n1 = parseInt(prompt("Digite o primeiro número: "));
+var a1 = parseInt(prompt("Digite o valor do primeiro ângulo: "));
 
-var n2 = parseInt(prompt("Digite o segundo número: "));
+var a2 = parseInt(prompt("Digite o valor do segundo ângulo: "));
+
+var a3 = parseInt(prompt("Digite o valor do Terceiro ângulo: "));
 
 
-if (n1 > 0 && n2 >0){
-  if (n1 % 2 == 0 && n2 % 2 == 0) {
-    console.log ("Os Números são Pares")
-  } else if ((n1 % 2 == 1 && n2 % 2 == 1)){
-    console.log ("Os Números são Impares")
-  } else {
-    console.log ("Existe um número par ou impar")
-  }
-} else {
-  console.log("Existe pelo menos um número negativo ou igual a Zero");
-}
+
