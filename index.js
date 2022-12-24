117eebc60c2caaf7ce9c68b9e5f58266a1214b42
Everton Rocha <everton.srocha@gmail.com>
2022-12-24 15:08:00 +0000

    Calculo de números entre 7 e 100 cuja divisão por 7, apenas aqueles que tem resto 3 são impressos na tela.

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const prompt = require('prompt-sync')();
-
-
-var a1 = parseInt(prompt("Digite o valor do primeiro ângulo: "));
-
-var a2 = parseInt(prompt("Digite o valor do segundo ângulo: "));
-
-var a3 = parseInt(prompt("Digite o valor do Terceiro ângulo: "));
-
-
-
+for (var i = 7; i <= 1000; i++) {
+  if (i % 7 == 3) {
+    console.log(i)
+  } else { }
+}
